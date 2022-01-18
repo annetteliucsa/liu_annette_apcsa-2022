a86a423cf20c10280d04b805bab4a99b345a536d
annetteliucsa <97622669+annetteliucsa@users.noreply.github.com>
2022-01-17 22:54:36 -0800

    UPLOADING UNIT 01 IT FINALLY WORKED

---
+++ Unit 01/src/StarsRunner.java
@@ -0,0 +1,20 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      // Hi omg
+      //call the methods needed to make the patterns on the word document
+      
+   }
+}
