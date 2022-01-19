ae99366877382e0b264903d7522256581f670253
annetteliucsa <97622669+annetteliucsa@users.noreply.github.com>
2022-01-18 22:42:59 -0800

    Finishing Unit 01

--- Unit01/src/StarsAndStripes.java
+++ Unit01/src/StarsAndStripes.java
@@ -18,21 +18,39 @@ public class StarsAndStripes
 
    public void printTwentyStars()
    {
+	   System.out.print("********************");
+	   System.out.println("");
    }
 
    public void printTwentyDashes()
    {
+	   System.out.print("--------------------");
+	   System.out.println("");
    }
 
    public void printTwoBlankLines()
    {
+	   System.out.println("");
+	   System.out.println("");
    }
    
    public void printASmallBox()
-   {	
+   {
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
    }
  
    public void printABigBox()
-   { 	
-   }   
+   {
+	   printASmallBox();
+	   printTwentyDashes();
+	   printTwentyStars();
+	   printTwentyDashes();
+	   printTwentyStars();
+   }
 }
--- Unit01/src/StarsRunner.java
+++ Unit01/src/StarsRunner.java
@@ -13,8 +13,11 @@ public class StarsRunner
    public static void main(String args[])
    {
       //instantiate a StarsAndStripes object
-      
+      StarsAndStripes abc = new StarsAndStripes();
       //call the methods needed to make the patterns on the word document
-      
+      abc.printASmallBox();
+	   abc.printTwoBlankLines();
+	   abc.printABigBox();
+	   abc.printTwentyDashes();
    }
 }
