095c0a0a021ba91d0a3408de7d75f5a2620b16c6
annetteliucsa <97622669+annetteliucsa@users.noreply.github.com>
2022-01-29 21:32:55 -0800

    Unit 06 upload

--- Unit05/src/StringRunner.java
+++ Unit05/src/StringRunner.java
@@ -17,8 +17,8 @@ public class StringRunner
 		System.out.println("Enter a string :: ");
 		String word = keyboard.nextLine();
 		StringOddOrEven myWord = new StringOddOrEven(word);
-		boolean myBool = myWord.isEven();
-		if(myBool == true) {
+		boolean myBoolean = myWord.isEven();
+		if(myBoolean == true) {
 			System.out.print(word + " is even.");
 		}
 		else System.out.print(word + " is odd.");
@@ -26,8 +26,8 @@ public class StringRunner
 		System.out.println("\nEnter a string :: ");
 		String wordOne = keyboard.nextLine();
 		StringOddOrEven myWord1 = new StringOddOrEven(wordOne);
-		boolean myBoolOne = myWord1.isEven();
-		if(myBoolOne == true) {
+		boolean myBooleanOne = myWord1.isEven();
+		if(myBooleanOne == true) {
 			System.out.print(wordOne + " is even.");
 		}
 		else System.out.print(wordOne + " is odd.");
@@ -35,8 +35,8 @@ public class StringRunner
 		System.out.println("\nEnter a string :: ");
 		String word2 = keyboard.nextLine();
 		StringOddOrEven myWord2 = new StringOddOrEven(word2);
-		boolean myBool2 = myWord2.isEven();
-		if(myBool2 == true) {
+		boolean myBoolean2 = myWord2.isEven();
+		if(myBoolean2 == true) {
 			System.out.print(word2 + " is even.");
 		}
 		else System.out.print(word2 + " is odd.");
@@ -44,8 +44,8 @@ public class StringRunner
 		System.out.println("\nEnter a string :: ");
 		String word3 = keyboard.nextLine();
 		StringOddOrEven myWord3 = new StringOddOrEven(word3);
-		boolean myBool3 = myWord3.isEven();
-		if(myBool3 == true) {
+		boolean myBoolean3 = myWord3.isEven();
+		if(myBoolean3 == true) {
 			System.out.print(word3 + " is even.");
 		}
 		else System.out.print(word3 + " is odd.");
@@ -53,8 +53,8 @@ public class StringRunner
 		System.out.println("\nEnter a string :: ");
 		String word4 = keyboard.nextLine();
 		StringOddOrEven myWord4 = new StringOddOrEven(word4);
-		boolean myBool4 = myWord4.isEven();
-		if(myBool4 == true) {
+		boolean myBoolean4 = myWord4.isEven();
+		if(myBoolean4 == true) {
 			System.out.print(word4 + " is even.");
 		}
 		else System.out.print(word4 + " is odd.");
@@ -62,8 +62,8 @@ public class StringRunner
 		System.out.println("\nEnter a string :: ");
 		String word5 = keyboard.nextLine();
 		StringOddOrEven myWord5 = new StringOddOrEven(word5);
-		boolean myBool5 = myWord5.isEven();
-		if(myBool5 == true) {
+		boolean myBoolean5 = myWord5.isEven();
+		if(myBoolean5 == true) {
 			System.out.print(word5 + " is even.");
 		}
 		else System.out.print(word5 + " is odd.");
@@ -71,8 +71,8 @@ public class StringRunner
 		System.out.println("\nEnter a string :: ");
 		String word6 = keyboard.nextLine();
 		StringOddOrEven myWord6 = new StringOddOrEven(word6);
-		boolean myBool6 = myWord.isEven();
-		if(myBool6 == true) {
+		boolean myBoolean6 = myWord.isEven();
+		if(myBoolean6 == true) {
 			System.out.print(word6 + " is even.");
 		}
 		else System.out.print(word6 + " is odd.");
@@ -80,8 +80,8 @@ public class StringRunner
 		System.out.println("\nEnter a string :: ");
 		String word7 = keyboard.nextLine();
 		StringOddOrEven myWord7 = new StringOddOrEven(word7);
-		boolean myBool7 = myWord7.isEven();
-		if(myBool7 == true) {
+		boolean myBoolean7 = myWord7.isEven();
+		if(myBoolean7 == true) {
 			System.out.print(word7 + " is even.");
 		}
 		else System.out.print(word7 + " is odd.");
