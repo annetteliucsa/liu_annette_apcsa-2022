026f18b5bea1530ba4402d65831f0e52b06a00f9
annetteliucsa <97622669+annetteliucsa@users.noreply.github.com>
2022-03-01 22:56:35 -0800

    Finished unit 11

--- Unit11/src/TriangleWordRunner.java
+++ Unit11/src/TriangleWordRunner.java
@@ -9,6 +9,9 @@ public class TriangleWordRunner
 {
 	public static void main( String args[] )
 	{
-		
+		System.out.println("Enter a word :: ");
+		Scanner keyboard = new Scanner(System.in);
+		String word = keyboard.nextLine();
+		TriangleWord.printTriangle(word);
 	}
 }
