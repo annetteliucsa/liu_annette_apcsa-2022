84f2a3b89afd3be2f79de3a4defd1f4628099912
annetteliucsa <97622669+annetteliucsa@users.noreply.github.com>
2022-03-24 23:07:49 -0700

    Update TotalRowRunner.java

--- Unit14/src/TotalRowRunner.java
+++ Unit14/src/TotalRowRunner.java
@@ -6,7 +6,10 @@ public class TotalRowRunner
 {
 	public static void main( String args[] ) throws Exception
 	{
-		//add code here			
+		//add code here	
+		TotalRow myRows = new TotalRow();
+		int[][] myList = {{1,2,3},{5,5,5,5}};
+		System.out.println(myRows.getRowTotals(myList));
 	}
 }
 
